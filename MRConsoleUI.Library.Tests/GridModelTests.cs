cbca7eef005498b8d543b1c70e6a46794b6a6d5a
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-10-25 19:47:51 +0100

    Add GridModel and Testing
    Added gridmodel and filing tests for it.

---
+++ MRConsoleUI.Library.Tests/GridModelTests.cs
@@ -0,0 +1,24 @@
+﻿using MRConsoleUI.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRConsoleUI.Library.Tests
+{
+    public class GridModelTests
+    {
+        [Theory]
+        [InlineData("5x5", 5, 5)]
+        public void GridModelDemensionsShouldBeAsExpectedValue(string grid, int expectedX, int expectedY)
+        {
+            List<int> expected = new List<int> { expectedX, expectedY };
+
+            GridModel model = new GridModel(grid);
+            var actual = model.Grid;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ MRConsoleUI.Library/Models/GridModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRConsoleUI.Library.Models
+{
+    public class GridModel
+    {
+        public List<int> Grid { get; set; }
+
+        public GridModel(string grid)
+        {
+
+        }
+    }
+}
