17731dc516d5da869017ebdbfe6b7885f94b2c3e
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-10-26 00:48:22 +0100

    Add Dependency Injection to ConsoleUI
    Added DI to our ConsoleUI
    Now application code goes to LogicService.cs.

---
+++ MRConsoleUI/LogicService.cs
@@ -0,0 +1,7 @@
+﻿public class LogicService : ILogicService
+{
+    public void Run()
+    {
+
+    }
+}
--- MRConsoleUI/Program.cs
+++ MRConsoleUI/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine();
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Configure Dependency injection
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddTransient<ILogicService, LogicService>();
+            })             
+            .Build();
+
+        // Start of the application
+        var svc = ActivatorUtilities.CreateInstance<LogicService>(host.Services);
+        svc.Run();
+    }
+}
