eb3ca227142e902b0fdfd4dda6b6a0128e1bc86b
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-10-26 05:57:10 +0100

    Refactor Move Method and Add More Tests.

--- MRConsoleUI.Library.Tests/RobotLogicTests.cs
+++ MRConsoleUI.Library.Tests/RobotLogicTests.cs
@@ -68,19 +68,73 @@ namespace MRConsoleUI.Library.Tests
 
         [Theory]
         [MemberData(nameof(DataList))]
-        public void MoveShouldReturnExpectedValue(List<int> expectedPosition, List<char> path)
+        public void MoveShouldReturnExpectedValue(List<int> expectedPosition, List<char> path, string dimensions)
         {
-            var rl = new RobotLogic();
-            List<int> robotCoordinates = new List<int> { 1, 1 };
+            List<int> actual= new List<int> { 1, 1 };
+            var movementLogic = new MovementLogic();
+            var direction = DirectionStatus.North;
+            GridModel grid = new GridModel(dimensions);
 
-            var actual = rl.Move();
+            foreach (var command in path)
+            {
+                switch (command)
+                {
+                    case 'F':
+                        movementLogic.MoveRobot(actual, direction, grid.Grid);
+                        break;
+                    case 'R':
+                        switch (direction)
+                        {
+                            case DirectionStatus.North:
+                                direction = DirectionStatus.East;
+                                break;
+                            case DirectionStatus.East:
+                                direction = DirectionStatus.South;
+                                break;
+                            case DirectionStatus.South:
+                                direction = DirectionStatus.West;
+                                break;
+                            case DirectionStatus.West:
+                                direction = DirectionStatus.North;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 'L':
+                        switch (direction)
+                        {
+                            case DirectionStatus.North:
+                                direction = DirectionStatus.West;
+                                break;
+                            case DirectionStatus.West:
+                                direction = DirectionStatus.South;
+                                break;
+                            case DirectionStatus.South:
+                                direction = DirectionStatus.East;
+                                break;
+                            case DirectionStatus.East:
+                                direction = DirectionStatus.North;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                }
+            }
 
             Assert.Equal(expectedPosition, actual);
         }
 
         public static IEnumerable<object[]> DataList => new List<object[]>
         {
-            new object[] { new List<int> { 4, 1 }, new List<char> { 'F', 'F', 'R', 'F', 'L', 'F', 'L', 'F' } }
+            new object[] { new List<int> { 4, 1 }, new List<char> { 'F', 'F', 'R', 'F', 'L', 'F', 'L', 'F' }, "5x5" },
+            new object[] { new List<int> { 2, 1 }, new List<char> { 'F' }, "5x5" },
+            new object[] { new List<int> { 1, 2 }, new List<char> { 'R', 'F' }, "15x5" },
+            new object[] { new List<int> { 1, 1 }, new List<char> { 'L', 'F' }, "5x54" },
+            new object[] { new List<int> { 1, 2 }, new List<char> { 'L', 'L', 'L', 'F' }, "5x5" },
+            new object[] { new List<int> { 2, 1 }, new List<char> { 'R', 'R', 'R', 'R', 'F' }, "55x55" },
+
         };
     } 
 }
--- MRConsoleUI.Library/RobotLogic.cs
+++ MRConsoleUI.Library/RobotLogic.cs
@@ -9,8 +9,11 @@ namespace MRConsoleUI.Library
 {
     public class RobotLogic
     {
+        private readonly IMovementLogic? _movementLogic;
         private GridModel? _grid;
         private List<char> _path;
+        private DirectionStatus _direction;
+        private List<int> _robotPosition;
 
         public RobotLogic()
         {
@@ -67,10 +70,58 @@ namespace MRConsoleUI.Library
             } while (validInput == false);
         }
 
-        public List<int> Move()
+        public void Move()
         {
-            return new List<int> { 4, 1};
-        }
+            _direction = DirectionStatus.North;
+            _robotPosition = new List<int> { 1, 1 };
 
+            foreach (var command in _path)
+            {
+                switch (command)
+                {
+                    case 'F':
+                        _movementLogic.MoveRobot(_robotPosition, _direction, _grid.Grid);
+                        break;
+                    case 'R':
+                        switch (_direction)
+                        {
+                            case DirectionStatus.North:
+                                _direction = DirectionStatus.East;
+                                break;
+                            case DirectionStatus.East:
+                                _direction = DirectionStatus.South;
+                                break;
+                            case DirectionStatus.South:
+                                _direction = DirectionStatus.West;
+                                break;
+                            case DirectionStatus.West:
+                                _direction = DirectionStatus.North;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 'L':
+                        switch (_direction)
+                        {
+                            case DirectionStatus.North:
+                                _direction = DirectionStatus.West;
+                                break;
+                            case DirectionStatus.West:
+                                _direction = DirectionStatus.South;
+                                break;
+                            case DirectionStatus.South:
+                                _direction = DirectionStatus.East;
+                                break;
+                            case DirectionStatus.East:
+                                _direction = DirectionStatus.North;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
     }
 }
