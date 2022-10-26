bc2cccdda992091b86c9ad7d1238c717eb18096c
Yaroslav Mechenko <mechenko.yaroslav@gmail.com>
2022-10-26 04:05:36 +0100

    Add RobotLogic and Failing tests.

---
+++ MRConsoleUI.Library.Tests/RobotLogicTests.cs
@@ -0,0 +1,25 @@
+﻿using MRConsoleUI.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRConsoleUI.Library.Tests
+{
+    public class RobotLogicTests
+    {
+        [Theory]
+        [InlineData("5x5", "5x5")]
+        public void AskForDimensionsShouldReturnExpectedValue(string expectedGrid, string actualGrid)
+        {
+            RobotLogic rl = new RobotLogic();
+            GridModel actual = new GridModel("1x1");
+            GridModel expected = new GridModel(expectedGrid);
+
+            rl.AskForDimensions(new GridModel(actualGrid));
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
